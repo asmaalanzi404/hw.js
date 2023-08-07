c67d17cdc5e924aed7e397e28082441fb064a96c
asma <asmaalanzi404@gmail.com>
2023-08-07 16:43:04 +0300

    lap

---
+++ mainScript.js
@@ -0,0 +1,38 @@
+
+for (let y = 1; y <=10; y--) {
+    x=y
+      console.log(x*x)
+    }//square
+
+for (let x = 1; x <= 20; x++) {
+    if (x % 2 == 0) {
+      console.log(x)
+    }}//even
+  
+
+for (let i = 1; i <= 25; i++) {
+    if (i % 2 !== 0) {
+      console.log(i)
+    }
+  }//odd
+
+let n=8
+let string=" "
+for(let T=1 ; T<=n;T++){
+ 
+  //for(let R=1;R<=T;R++){
+    for(let l=1 ; l<=T;l++){
+     string+=l;
+    }
+    string+="\n" 
+}
+console.log(string);
+
+
+
+
+
+
+
+
+
